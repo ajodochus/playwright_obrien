98be318bd0d6b6b8a74369ce032dbc0c323797d4
ajodochus <ajodochus@gmail.com>
2023-09-17 21:51:53 +0200

    working

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,18 +2,10 @@ import { defineConfig, devices } from '@playwright/test';
 import { profileEnd } from 'console';
 import dotenv from 'dotenv';
 import path from 'path';
-
-//dotenv.config({ path: path.resolve(__dirname, '..', 'api.env') });
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
 dotenv.config();
-//require('dotenv').config()
+//dotenv.config({ path: path.resolve(__dirname, '..', 'api.env') });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+
 export default defineConfig({
   testDir: './tests-examples',
   /* Run tests in files in parallel */
--- tests-examples/api.spec.ts
+++ tests-examples/api.spec.ts
@@ -1,21 +1,52 @@
 import { test, expect, type Page } from '@playwright/test';
-var randomstring = require("randomstring");
-const REPO = 'test';
+const REPO = 'repo_for_playwright';
 const USER = 'ajodochus';
 
-test('should create a bug report', async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: randomstring.generate(5),
-      body: 'Bug ' + randomstring.generate(3),
+test('API test', async ({ page, request }) => {
+  await test.step(`delete repo if exists `, async () => {
+    const response1 = await request.delete(`/repos/${USER}/${REPO}`);
+    if(response1.ok()){
+      console.log("repo deleted");
+    } else {
+      console.log("repo does not exist");
     }
   });
-  expect(newIssue.ok()).toBeTruthy();
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Bug] report 1',
-    body: 'Bug description'
-  }));
+  await test.step(`Create github item with api `, async () => {
+    // Create a new repository
+    const response = await request.post('/user/repos', {
+      data: {
+        name: REPO
+      }
+    });
+    expect(response.ok()).toBeTruthy();
+  });
+
+
+  await test.step(`Create github bug item with api `, async () => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+      data: {
+        title: '[Bug] report 1',
+        body: 'Bug description',
+      }
+    });
+    expect(newIssue.ok()).toBeTruthy();
+  });
+
+  await test.step(`check github item with api `, async () => {
+    const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
+    expect(issues.ok()).toBeTruthy();
+    expect(await issues.json()).toContainEqual(expect.objectContaining({
+      title: '[Bug] report 1',
+      body: 'Bug description'
+    }));
+  });
+
+  await test.step(`check github item with api `, async () => {
+
+  });
+
 });
+
+
+
