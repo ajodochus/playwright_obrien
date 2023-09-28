90bc9e7200a4d1458b97a9c2046b1757c176b763
ajodochus <ajodochus@gmail.com>
2023-09-28 22:42:45 +0200

    ökj

--- joomla_and_playwright/tests/login_user.spec.ts
+++ joomla_and_playwright/tests/login_user.spec.ts
@@ -1,18 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.use({
+  baseURL: process.env.URL_JOOMLA_FE
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('login as user1 and store session', async ({ page }) => {
+  await page.goto('/');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill(process.env.USER_USER1 as string);
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD_USER1 as string);
+  await page.locator('button[name="Submit"]').click();
+  await page.getByRole('link', { name: 'Nav 1 (eng)' }).click();
 });
+
