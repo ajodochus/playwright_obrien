b64d7400d7aca9826108f0749eaea2ea754e528a
ajodochus <ajodochus@gmail.com>
2023-12-02 18:14:00 +0100

    new ssh helper class

--- joomla_and_playwright/tests/ssh2.spec.ts
+++ joomla_and_playwright/tests/ssh2.spec.ts
@@ -1,37 +1,8 @@
 import { test, expect } from '@playwright/test';
-import SshConnection from '../utils/SshConnection';
-
-// Replace these values with your remote server details
-const remoteServer = {
-  host: '192.168.178.104',
-  port: 22, // default SSH port
-  username: 'ajodochus',
-  password: 'aj.123',
-};
-
-// Replace this value with your shell command
+import { executeSSH } from '../utils/SshHelper';
 const commandToExecute = 'mysqldump -ujoom -paj.123 -h192.168.178.104 --compact joomla_db < ./dump_3.sql';
 
-// Create an instance of SshConnection
-const sshConnection = new SshConnection(remoteServer);
-
-// Connect to the remote server
-sshConnection
-  .connect()
-  .then(() => {
-    // Execute the shell command
-    return sshConnection.executeCommand(commandToExecute);
-  })
-  .then((result) => {
-    console.log(`Command output:\n${result.output}`);
-  })
-  .catch((err) => {
-    console.error('Error:', err);
-  })
-  .finally(() => {
-    // Close the SSH connection
-    sshConnection.end();
-  });
+(async () => console.log(await executeSSH('ls -al')))();
 
 
 
---
+++ joomla_and_playwright/utils/SshHelper.ts
@@ -0,0 +1,44 @@
+import { Client } from 'ssh2';
+
+export function executeSSH(command: string): Promise<string> {
+  const config = {
+    host: '192.168.178.104',
+  port: 22, // default SSH port
+  username: 'ajodochus',
+  password: 'aj.123',
+  };
+
+  return new Promise((resolve, reject) => {
+    const conn = new Client();
+
+    conn.on('ready', () => {
+      console.log('SSH connection established');
+
+      conn.exec(command, (err, stream) => {
+        if (err) {
+          conn.end();
+          return reject(err);
+        }
+
+        let output = '';
+
+        stream
+          .on('close', (code, signal) => {
+            console.log(`Command execution closed with code ${code} and signal ${signal}`);
+            conn.end();
+            resolve(output);
+          })
+          .on('data', (data) => {
+            output += data;
+          })
+          .stderr.on('data', (data) => {
+            console.error(`Error output: ${data}`);
+          });
+      });
+    });
+
+    conn.connect(config);
+  });
+
+  
+}
