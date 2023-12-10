38c962ba1b63a4c9754906e14b9aac78fc43869e
ajodochus <ajodochus@gmail.com>
2023-12-10 21:47:27 +0100

    using axios for api requests

--- joomla_and_playwright/tests/smtp_mailslurper.spec.ts
+++ joomla_and_playwright/tests/smtp_mailslurper.spec.ts
@@ -1,6 +1,7 @@
 import { PS_Command } from "../utils/powershell_handler";
 import { test, expect } from '@playwright/test';
-import { send_email } from "../utils/mailslurper";
+import { get_all_mails, send_email, delete_all_mails } from "../utils/mailslurper";
+
 
 
 // before each test, start mailslurper
@@ -10,15 +11,17 @@ test.beforeAll(async () => {
 
 // after each test, stop mailslurper
 test.afterAll(async () => {
-  await PS_Command("stop mailslurper");
+  //await PS_Command("stop mailslurper");
 });
 
-test('send first mail', async ({ page }) => {
+test.skip('send first mail', async ({ page }) => {
   test.info().annotations.push({ type: 'email', description: 'first message' });
   await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my first subject', text: 'This is my first text' });
 });
 
 test('send 2nd mail', async ({ page }) => {
   test.info().annotations.push({ type: 'email', description: '2nd message' });
-  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my 2nd subject', text: 'this is my 2nd text' });
+  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: '2ndsubject', text: 'this is my 2nd text' });
+  await get_all_mails();
+  await delete_all_mails();
 });
--- joomla_and_playwright/utils/mailslurper.ts
+++ joomla_and_playwright/utils/mailslurper.ts
@@ -1,9 +1,12 @@
 import nodemailer from 'nodemailer';
 import { exec, spawn } from 'child_process';
-
+import { APIRequestContext } from '@playwright/test';
+import exp from 'constants';
+import anxios from 'axios';
 const processName = 'mailslurper';
 const timeoutDuration = 10000; // 10 seconds
 
+// api: https://github.com/mailslurper/mailslurper/wiki/API-Guide
 
 const transporter = nodemailer.createTransport({
   host: '127.0.0.1',
@@ -14,9 +17,6 @@ const transporter = nodemailer.createTransport({
     pass: 'test.123',
   },
 });
-
-// Define the email options
-
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -34,7 +34,6 @@ export async function send_email(mailOptions) {
     };
   }
 
-  await sleep(10000);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email:', error);
@@ -42,5 +41,41 @@ export async function send_email(mailOptions) {
       console.log('Email sent:', info.response);
     }
   });
+  await sleep(10000);
 }
 
+
+// Import the axios library
+import axios from 'axios';
+
+// Define the API endpoint URL
+const apiUrl =  'http://127.0.0.1:8085/mail';
+// Define the data you want to send in the request body
+const requestData = {
+  'pruneCode': 'all'
+};
+
+// Use async/await to make the request
+export async function delete_all_mails() {
+  axios.delete(apiUrl, { data: requestData })
+  .then(response => {
+    // Handle the response
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
+}
+
+export async function get_all_mails() {
+  axios.get(apiUrl)
+  .then(response => {
+    // Handle the response
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
+}
