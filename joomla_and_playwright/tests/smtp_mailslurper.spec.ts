4008b2d5c299cd4debb9d1653d76ab4e9e640a65
ajodochus <ajodochus@gmail.com>
2023-12-10 19:00:42 +0100

    mailoptions as attribute

--- joomla_and_playwright/tests/smtp_mailslurper.spec.ts
+++ joomla_and_playwright/tests/smtp_mailslurper.spec.ts
@@ -6,8 +6,8 @@ import { send_email } from "../utils/mailslurper";
 
 
 test('test', async ({ page }) => {
-    test.info().annotations.push({ type: 'email', description: 'meilslurper' });
-    await PS_Command("start mailslurper");
-   await send_email();
-    await PS_Command("stop mailslurper");
-  });
+  test.info().annotations.push({ type: 'email', description: 'meilslurper' });
+  await PS_Command("start mailslurper");
+  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my subject', text: 'This is a new text' });
+  await PS_Command("stop mailslurper");
+});
--- joomla_and_playwright/utils/mailslurper.ts
+++ joomla_and_playwright/utils/mailslurper.ts
@@ -4,64 +4,43 @@ import { exec, spawn } from 'child_process';
 const processName = 'mailslurper';
 const timeoutDuration = 10000; // 10 seconds
 
-function waitForProcess() {
-    const processTimeout = setTimeout(() => {
-      console.error(`Timeout: Process '${processName}' did not exist within ${timeoutDuration / 1000} seconds.`);
-      process.exit(1); // You can handle this according to your use case
-    }, timeoutDuration);
-  
-    const checkProcess = spawn('tasklist', ['/FI', `IMAGENAME eq ${processName}.exe`]);
-  
-    checkProcess.stdout.on('data', (data) => {
-      const output = data.toString();
-      if (output.includes(processName)) {
-        console.log(`Process '${processName}' found!`);
-        clearTimeout(processTimeout);
-        // Do something when the process is found, e.g., proceed with your logic
-      }
-    });
-  
-    checkProcess.on('exit', (code) => {
-      if (code !== 0) {
-        console.error(`Error checking process. Exit code: ${code}`);
-        clearTimeout(processTimeout);
-        process.exit(1);
-      }
-    });
-  }
 
 const transporter = nodemailer.createTransport({
-    host: '127.0.0.1',
-    port: 587,
-    secure: false, // Set to true if using a secure connection (e.g., port 465)
-    auth: {
-        user: 'test',
-        pass: 'test.123',
-    },
+  host: '127.0.0.1',
+  port: 587,
+  secure: false, // Set to true if using a secure connection (e.g., port 465)
+  auth: {
+    user: 'test',
+    pass: 'test.123',
+  },
 });
 
 // Define the email options
-const mailOptions = {
-    from: 'sender@example.com',
-    to: 'recipient@example.com',
-    subject: 'Test Email',
-    text: 'This is a test email.',
-};
+
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-export async function send_email() {
-    await sleep(10000);
+export async function send_email(mailOptions) {
+  if (!mailOptions) {
+    mailOptions = {
+      from: 'sender@example.com',
+      to: 'recipient@example.com',
+      subject: 'Test Email',
+      text: 'This is a test email.',
+    };
+  }
+
+  await sleep(10000);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-        console.error('Error sending email:', error);
+      console.error('Error sending email:', error);
     } else {
-        console.log('Email sent:', info.response);
+      console.log('Email sent:', info.response);
     }
-});
+  });
 }
 
