ddeead8ff45f194aaf422731c7cf2a60c91d185f
ajodochus <ajodochus@gmail.com>
2023-12-10 19:08:01 +0100

    send multiple emails

--- joomla_and_playwright/tests/smtp_mailslurper.spec.ts
+++ joomla_and_playwright/tests/smtp_mailslurper.spec.ts
@@ -3,11 +3,22 @@ import { test, expect } from '@playwright/test';
 import { send_email } from "../utils/mailslurper";
 
 
-
-
-test('test', async ({ page }) => {
-  test.info().annotations.push({ type: 'email', description: 'meilslurper' });
+// before each test, start mailslurper
+test.beforeAll(async () => {
   await PS_Command("start mailslurper");
-  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my subject', text: 'This is a new text' });
+});
+
+// after each test, stop mailslurper
+test.afterAll(async () => {
   await PS_Command("stop mailslurper");
+});
+
+test('send first mail', async ({ page }) => {
+  test.info().annotations.push({ type: 'email', description: 'first message' });
+  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my first subject', text: 'This is my first text' });
+});
+
+test('send 2nd mail', async ({ page }) => {
+  test.info().annotations.push({ type: 'email', description: '2nd message' });
+  await send_email({ from: 'test@test.de', to: 'to@test.de', subject: 'my 2nd subject', text: 'this is my 2nd text' });
 });
